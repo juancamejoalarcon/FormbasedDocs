92b2afb7ff89bd1d5aa9a1f9cb32955ab5eed600
juancamejoalarcon <juancamejo93@hotmail.com>
2019-10-01 22:01:36 +0200

    sdfasdf

--- client/src/app/modules/certified-forms/forms/contrato-arras-penitenciales/step-model.service.ts
+++ client/src/app/modules/certified-forms/forms/contrato-arras-penitenciales/step-model.service.ts
@@ -7,20 +7,69 @@ export class StepModelService {
 
   constructor() { }
 
-  test() {
-    console.log('funciona');
-  }
-
   init(steps: Array<Object>) {
     this.steps = steps;
+  }
+
+  input(replacement: string, inputType: string, identifier: string) {
+    // 1. Find the step
+    this.steps.forEach((step) => {
+      if (step.identifier === identifier) {
+        step.replacement = replacement;
+      }
+    });
+  }
+
+  buildForEach(value: number, identifier: string) {
+    // 1. Find the step
+    this.steps.forEach((step, index) => {
+      if (step.identifier === identifier) {
+        step.value = value;
+        // 2. Clean possible previously added steps, so we don't repeat them
+        console.log(this.steps[index + 1].identifier);
+        console.log(step.identifier);
+        while (this.steps[index + 1] && this.steps[index + 1].identifier.includes(step.identifier)) {
+          this.steps.splice((index + 1), 1);
+        }
+        // 3. Loop through the texts that will be inserted
+        step.content.forEach((content) => {
+          // 4. Add steps
+          for (let i = 0; i < value; i++) {
+            content.subSteps.forEach((subStep, subStepIndex) => {
+              this.steps.splice(((index + 1) + subStepIndex ), 0, subStep);
+            });
+          }
+          // 5. Insert text in the office document
+        });
+      }
+    });
     console.log(this.steps);
   }
 
-  input(valueToReplace: string, inputType: string, identifier: string) {
-    this.steps.forEach((step) => { 
-      if (step.identifier) {
-        console.log(step);
+  onInputRadioCSelected(radioSelectedId: any, identifier: string) {
+    // 1. Find the step
+    this.steps.forEach((step, index) => {
+      if (step.identifier === identifier) {
+        // 2. Find radio selected
+        step.radios.forEach((radio) => {
+          if (radio.radioId === radioSelectedId) {
+            // 3. Clean possible previously added steps, so we don't repeat them
+            while (this.steps[index + 1] && this.steps[index + 1].identifier.includes(step.identifier)) {
+              this.steps.splice((index + 1), 1);
+            }
+            // 2. Add steps
+            radio.subSteps.forEach((subStep, subStepIndex) => {
+              this.steps.splice(((index + 1) + subStepIndex ), 0, subStep);
+            });
+          } else {
+            radio.checked = false;
+          }
+        });
       }
-    })
+    });
+  }
+
+  getStepsModel() {
+    return this.steps;
   }
 }
--- client/src/app/modules/certified-forms/forms/contrato-arras-penitenciales/steps.ts
+++ client/src/app/modules/certified-forms/forms/contrato-arras-penitenciales/steps.ts
@@ -7,7 +7,104 @@ export const steps = [
     },
     {
       type: 'iText',
-      identifier: 'localidad'
+      identifier: 'iLocalidad',
+      replacement: '',
+    },
+    {
+      type: 'iDate',
+      identifier: 'iFecha',
+      replacement: '',
+    },
+    {
+      type: 'iForEach',
+      value: 1,
+      replacement: '',
+      identifier: 'iParteVendedora',
+      content: [
+        {
+          identifier: 'iParteVendedoraOptionBorrar1',
+          text: 'Esto es una vaina de prueba marico iParteVendedoraOptionBorrar1Prueba',
+          subSteps: [
+            {
+              type: 'iText',
+              identifier: 'iParteVendedoraOptionBorrar1Prueba',
+              replacement: '',
+            }
+          ]
+        }
+        // {
+        //   identifier: 'iForEachOptionBorrar2',
+        //   text: 'Pendejo iForEachOptionBorrar2Prueba',
+        //   subSteps: [
+        //     {
+        //       type: 'iText',
+        //       identifier: 'iForEachOptionBorrar2Prueba',
+        //       replacement: '',
+        //     }
+        //   ]
+        // }
+      ]
+    },
+    {
+      question: 'Cuestión condicional',
+      wordToReplace: 'iPrueba',
+      identifier: 'iPrueba',
+      type: 'iRadioC',
+      replacement: '',
+      radios: [
+      {
+        label: 'Opción a: Persona física',
+        radioId: 'iPruebaOptionA',
+        value: `- iNombre, persona física, mayor de edad, de estado civil iEstado`,
+        checked: false,
+        subSteps: [
+        {
+          question: '¿Nombre de la persona?',
+          wordToReplace: 'iNombre',
+          identifier: 'iPruebaOptionAiNombre',
+          type: 'inputText',
+          replacement: '',
+          isFocused: false,
+          mandatory: true
+        },
+        {
+          question: '¿Estado Civil?',
+          wordToReplace: 'iEstado',
+          identifier: 'iPruebaOptionAiEstado',
+          type: 'inputText',
+          replacement: '',
+          isFocused: false,
+          mandatory: true
+        },
+      ]
+      },
+      {
+        label: 'Opción b: Persona jurídica',
+        value: `- iNombre, persona jurídica, con domicilio en iEstado`,
+        radioId: 'iPruebaOptionB',
+        checked: false,
+        subSteps: [
+          {
+            question: '¿Nombre de la empresa?',
+            wordToReplace: 'iNombre',
+            identifier: 'iPruebaOptionBiNombre',
+            type: 'inputText',
+            replacement: '',
+            isFocused: false,
+            mandatory: true
+          },
+          {
+            question: '¿Domicilio fiscal de la empresa?',
+            wordToReplace: 'iEstado',
+            identifier: 'iPruebaOptionBiEstado',
+            type: 'inputText',
+            replacement: '',
+            isFocused: false,
+            mandatory: true
+          },
+        ]
+      },
+    ],
     },
     {
         type: 'end',
