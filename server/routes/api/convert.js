ed91431d258b056464bdcd1b2458d30b154cb1de
juancamejoalarcon <juancamejo93@hotmail.com>
2019-10-20 19:41:23 +0200

    dsdfg

--- server/routes/api/convert.js
+++ server/routes/api/convert.js
@@ -5,10 +5,6 @@ const exec = require('child_process').exec;
 
 // var convert = function (file, cb) {
 //     try {
-//         var p = path.parse(file);
-//         var pdf = path.join(p.dir, p.name) + '.pdf';
-//         var cmd = 'soffice --headless --convert-to pdf --outdir "' + p.dir + '" "' + file + '"';
-//         console.info(cmd);
 //         exec(cmd, function (err, stdout, stderr) {
 //             if (err) {
 //                 cb(err);
@@ -20,12 +16,9 @@ const exec = require('child_process').exec;
 //                 cb("not exist");
 //             }
 //         });
-//     } catch (err) {
-//         cb(err);
-//     }
 // };
 
-router.post('/', function(req, res) {
+router.post('/toPdf', function(req, res) {
     // const cmd = '/Applications/LibreOffice.app/Contents/MacOS/soffice --headless -env:UserInstallation=file:///tmp/LibreOffice_Conversion_${Juan} --convert-to pdf:writer_pdf_Export --outdir ./tmp/pdfs /Users/Juan/Downloads/cosa.odt';
     const name = req.body.fileName;
     let odt = req.body.doc;
@@ -35,7 +28,7 @@ router.post('/', function(req, res) {
         if(err) {
             return console.log(err);
         }
-        const cmd = '/Applications/LibreOffice.app/Contents/MacOS/soffice --headless -env:UserInstallation=file:///tmp/LibreOffice_Conversion_${Juan} --convert-to pdf:writer_pdf_Export --outdir' 
+        const cmd = '/Applications/LibreOffice.app/Contents/MacOS/soffice --headless -env:UserInstallation=file:///tmp/LibreOffice_Conversion_${Juan} --convert-to pdf:writer_pdf_Export --outdir';
         + ` ./tmp/pdfs ./tmp/${name}.odt`;
         exec(cmd, function (err, stdout, stderr) {
             if (err) {
@@ -50,5 +43,42 @@ router.post('/', function(req, res) {
         });
     }); 
 });
+
+router.post('/toWord', function(req, res) {
+    const name = req.body.fileName;
+    let odt = req.body.doc;
+    odt = odt.replace('data:application/vnd.oasis.opendocument.text;base64,', '');
+
+    fs.writeFile(`./tmp/${name}.odt`, odt, 'base64', function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        const cmd = '/Applications/LibreOffice.app/Contents/MacOS/soffice --headless -env:UserInstallation=file:///tmp/LibreOffice_Conversion_${Juan} --convert-to doc --outdir'
+        + ` ./tmp/words ./tmp/${name}.odt`;
+        exec(cmd, function (err, stdout, stderr) {
+            if (err) {
+                console.log('exec error: ', err);
+                return;
+            }
+            var file = path.basename(`./tmp/words/${name}.doc`);
+            const data = fs.readFileSync(`./tmp/words/${name}.doc`);
+            return res.json({
+                word: 'data:application/msword;base64,'+ data.toString('base64')
+            });
+            return res.json({});
+        });
+    });
+
+    // const cmd = '/Applications/LibreOffice.app/Contents/MacOS/soffice --headless -env:UserInstallation=file:///tmp/LibreOffice_Conversion_${Juan} --convert-to doc --outdir ./tmp/words /Users/Juan/Downloads/cosa.odt';
+    // exec(cmd, function (err, stdout, stderr) {
+    //     if (err) {
+    //         console.log('exec error: ', err);
+    //         return;
+    //     }
+    //     return res.json({});
+    // });
+
+});
+
 
 module.exports = router;
