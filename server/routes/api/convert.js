c74830ee3b52e64aa59d2808bb5c1196b0dcfbd3
juancamejoalarcon <juancamejo93@hotmail.com>
2019-10-20 18:48:30 +0200

    libre

--- server/routes/api/convert.js
+++ server/routes/api/convert.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-var fs = require('fs');
-var path = require('path');
-var exec = require('child_process').exec;
+const fs = require('fs');
+const path = require('path');
+const exec = require('child_process').exec;
 
 // var convert = function (file, cb) {
 //     try {
@@ -25,21 +25,30 @@ var exec = require('child_process').exec;
 //     }
 // };
 
-router.get('/', function(req, res) {
-    const cmd = '/Applications/LibreOffice.app/Contents/MacOS/soffice --headless -env:UserInstallation=file:///tmp/LibreOffice_Conversion_${Juan} --convert-to pdf:writer_pdf_Export --outdir ${HOME}/lo_pdfs /Users/Juan/Downloads/cosa.odt';
-    console.info(cmd);
-    exec(cmd, function (err, stdout, stderr) {
-        if (err) {
-            console.log('exec error: ', err);
-            return;
+router.post('/', function(req, res) {
+    // const cmd = '/Applications/LibreOffice.app/Contents/MacOS/soffice --headless -env:UserInstallation=file:///tmp/LibreOffice_Conversion_${Juan} --convert-to pdf:writer_pdf_Export --outdir ./tmp/pdfs /Users/Juan/Downloads/cosa.odt';
+    const name = req.body.fileName;
+    let odt = req.body.doc;
+    odt = odt.replace('data:application/vnd.oasis.opendocument.text;base64,', '');
+
+    fs.writeFile(`./tmp/${name}.odt`, odt, 'base64', function(err) {
+        if(err) {
+            return console.log(err);
         }
-        // if (fs.existsSync(pdf)) {
-        //     cb(null, pdf);
-        // } else {
-        //     cb("not exist");
-        // }
-        return res.json({});
-    });
+        const cmd = '/Applications/LibreOffice.app/Contents/MacOS/soffice --headless -env:UserInstallation=file:///tmp/LibreOffice_Conversion_${Juan} --convert-to pdf:writer_pdf_Export --outdir' 
+        + ` ./tmp/pdfs ./tmp/${name}.odt`;
+        exec(cmd, function (err, stdout, stderr) {
+            if (err) {
+                console.log('exec error: ', err);
+                return;
+            }
+            var file = path.basename(`./tmp/pdfs/${name}.pdf`);
+            const data = fs.readFileSync(`./tmp/pdfs/${name}.pdf`);
+            return res.json({
+                pdf: 'data:application/pdf;base64,'+ data.toString('base64')
+            });
+        });
+    }); 
 });
 
 module.exports = router;
