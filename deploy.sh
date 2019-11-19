0848558b7734ff7daf75c629e96f669620a98fa2
CAMEJO ALARCON <JUAN.CAMEJO.ALARCON.ext@ing.com>
2019-11-19 14:27:03 +0100

    Changed sh file

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,23 @@
 ############ CONSTANTS ###########
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+BLUE='\033[0;34m'
 NC='\033[0m' # No Color
+
+BLUEbg='\e[44m'
+NCbg='\e[49m' # No Color
+
 ##################################
 
 # SWITCH TO DEV BRANCH
-switch_to_dev_branch() {
+check_if_current_branch_correct() {
     currentBranch=$(git rev-parse --abbrev-ref HEAD)
-    echo "Current branch: ${currentBranch}"
-    if [[ ${currentBranch} != "dev" ]]; then
-        git checkout dev
+    if [[ ${currentBranch} == $1 ]]; then
+        return 1
+     else
+        return 0
     fi
-    echo -e "${GREEN}Success:${NC} On dev branch"
+    # echo -e "${GREEN}Success:${NC} On $1 branch"
 }
 
 # CHECK IF THERE ARE ANY COMMITS
@@ -46,9 +52,29 @@ build_and_deploy() {
     git subtree push --prefix server heroku master
 }
 
-# switch_to_dev_branch
-# if are_uncommited_changes == 0; then
-#     echo -e "${GREEN}Success:${NC} Changes are commited"
-# fi
+environment=$1
 
-echo "First arg: $1"
+# DEVELOPMENT
+if [ "$environment" = 'dev' ]; then 
+    echo -e "####### START DEPLOY IN DEVELOPMENT ########"
+    if check_if_current_branch_correct 'dev' == 0; then
+        echo -e "${RED}Error:${NC} you are not in Dev branch"
+    else
+        echo -e "${GREEN}Success:${NC} Correct branch"
+        if are_uncommited_changes == 0; then
+            echo -e "${GREEN}Success:${NC} Changes are commited"
+        fi
+    fi
+
+# PRODUCTION
+elif [ "$environment" = 'prod' ]; then 
+    echo -e "####### START DEPLOY IN PRODUCTION ########"
+    if check_if_current_branch_correct 'prod' == 0; then
+        echo -e "${RED}Error:${NC} you are not in Master branch"
+    else
+        echo -e "${GREEN}Success:${NC} Correct branch"
+        if are_uncommited_changes == 0; then
+            echo -e "${GREEN}Success:${NC} Changes are commited"
+        fi
+    fi
+fi
