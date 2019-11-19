e33a4a5c531ce0b446994e0fcb3616af1264dced
CAMEJO ALARCON <JUAN.CAMEJO.ALARCON.ext@ing.com>
2019-11-19 08:39:39 +0100

    sh modified

--- .sh
+++ .sh
@@ -1,10 +1,25 @@
-git checkout dev
-pushd ./client
-ng build
-popd
-pushd ./server
-ls
-popd
-git add .
-git commit -m "build for deploy"
-git subtree push --prefix server heroku master
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# git checkout dev
+# pushd ./client
+# ng build
+# popd
+# pushd ./server
+# ls
+# popd
+# git add .
+# git commit -m "build for deploy"
+# git subtree push --prefix server heroku master
+
+# CHECK IF THERE ARE ANY COMMITS
+# gitStatus=$(git status)
+# if [[ $gitStatus == *"Changes not staged for commit"* ]]; then
+#     echo -e "${RED}Error:${NC} Uncommited changes. Please commit, and then push to Heroku"
+# elif [[ $gitStatus == *"Changes to be committed"* ]]; then
+#     echo -e "${RED}Error:${NC} Uncommited changes. Please commit, and then push to Heroku"
+# fi
+
+# SWITCH TO DEV BRANCH
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
+echo "${currentBranch}"
