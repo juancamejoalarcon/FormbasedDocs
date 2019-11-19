a53d94c50f9ea1ac5fe5997889524078843046f0
juancamejoalarcon <juancamejo93@hotmail.com>
2019-11-19 19:55:15 +0100

    fixing for deploy

--- deploy.sh
+++ deploy.sh
@@ -43,7 +43,7 @@ are_uncommited_changes() {
 build_and_deploy() {
     # Build angular project
     pushd ./client
-    ng build --configuration=prod
+    ng build --configuration=production
     popd
     # Rewrite commands of Procfile
     pushd ./server
