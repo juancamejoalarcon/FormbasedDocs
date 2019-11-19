7c26a029a0e8b5a09d3e2f6bd7ce3cc93e0c9771
CAMEJO ALARCON <JUAN.CAMEJO.ALARCON.ext@ing.com>
2019-11-19 09:17:50 +0100

    modified sh file

--- .sh
+++ .sh
@@ -1,4 +1,5 @@
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 # git checkout dev
@@ -21,5 +22,29 @@ NC='\033[0m' # No Color
 # fi
 
 # SWITCH TO DEV BRANCH
-currentBranch=$(git rev-parse --abbrev-ref HEAD)
-echo "${currentBranch}"
+switch_to_dev_branch() {
+    currentBranch=$(git rev-parse --abbrev-ref HEAD)
+    echo "Current branch: ${currentBranch}"
+    if [[ ${currentBranch} != "dev" ]]; then
+        git checkout dev
+    fi
+    echo -e "${GREEN}Success:${NC} On dev branch"
+}
+
+are_uncommited_changes() {
+    gitStatus=$(git status)
+    if [[ $gitStatus == *"Changes not staged for commit"* ]]; then
+        echo -e "${RED}Error:${NC} Uncommited changes. Please commit, and then push to Heroku"
+        return 1
+    elif [[ $gitStatus == *"Changes to be committed"* ]]; then
+        echo -e "${RED}Error:${NC} Uncommited changes. Please commit, and then push to Heroku"
+        return 1
+    else
+        return 0
+    fi
+}
+
+switch_to_dev_branch
+if are_uncommited_changes == 0; then
+    echo -e "${GREEN}Success:${NC} Changes are commited"
+fi
