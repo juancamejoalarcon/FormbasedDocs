d2d02fda92fc38952ba00f89e0a8a2e1de802a1b
juancamejoalarcon <juancamejo93@hotmail.com>
2019-12-25 22:33:57 +0100

    Modified templates for emails

--- server/helpers/mails/mail.js
+++ server/helpers/mails/mail.js
@@ -2,19 +2,20 @@ const nodemailer = require('nodemailer');
 const certifiedForms = require('../certified-forms').certifiedForms;
 const convert = require('../convert').convert;
 const mailStrings = require('./mailStrings');
+const smtp = {
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'formbaseddocs@formbaseddocs.com', // generated ethereal user
+        pass: 'ycgvjiggefsdmkwb' // generated ethereal password
+    }
+};
 
 const emailSender =  {
     checkoutConfirm: (email, transactionId, formType, date, uri) => {
         // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: 'formbaseddocs@formbaseddocs.com', // generated ethereal user
-                pass: '23bocomfi' // generated ethereal password
-            }
-        });
+        let transporter = nodemailer.createTransport(smtp);
 
         let today = new Date(date);
         const dd = String(today.getDate()).padStart(2, '0');
@@ -28,44 +29,39 @@ const emailSender =  {
                 formName = form.title;
                 amount = form.amount;
             }
-        })
+        });
 
-        convert.toPdf(formType, uri).then((file) => {
-            console.log('------');
-            console.log(file);
-
-            // send mail with defined transport object
-            let info = transporter.sendMail({
-                from: '<formbaseddocs@formbaseddocs.com>', // sender address
-                to: `${email}, formbaseddocs@formbaseddocs.com`, // list of receivers
-                subject: `Formbaseddocs - ${formName}`, // Subject line
-                text: `Formbaseddocs - ${formName}`, // plain text body
-                html: mailStrings.invoice(transactionId, today, formName, amount),
-                attachments: [{
-                    filename: `${formType}.pdf`,
-                    path: file,
-                    contentType: 'application/pdf'
-                }],
-            });
-
-            console.log('Message sent: %s', info.messageId);
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        convert.toWord(formType, uri).then((wordFile) => {
+            convert.toPdf(formType, uri).then((file) => {
+                console.log('------');
+                console.log(file);
     
-            // Preview only available when sending through an Ethereal account
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        })
+                // send mail with defined transport object
+                let info = transporter.sendMail({
+                    from: '<formbaseddocs@formbaseddocs.com>', // sender address
+                    to: `${email}, formbaseddocs@formbaseddocs.com`, // list of receivers
+                    subject: `Formbaseddocs - ${formName}`, // Subject line
+                    text: `Formbaseddocs - ${formName}`, // plain text body
+                    html: mailStrings.invoice(transactionId, today, formName, amount),
+                    attachments: [
+                    {
+                        filename: `${formType}.pdf`,
+                        path: file,
+                        contentType: 'application/pdf'
+                    },
+                    {
+                        filename: `${formType}.doc`,
+                        path: wordFile,
+                        contentType: 'application/msword'
+                    }
+                ],
+                });
+    
+            });
+        });
     },
     forgotPassword: (email, user, token) => {
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: 'formbaseddocs@formbaseddocs.com', // generated ethereal user
-                pass: '23bocomfi' // generated ethereal password
-            }
-        });
+        let transporter = nodemailer.createTransport(smtp);
 
         // send mail with defined transport object
         let info = transporter.sendMail({
