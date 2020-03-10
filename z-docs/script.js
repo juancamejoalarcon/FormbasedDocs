646de61a46abeeeb7f8fe44f76dbd906ca7315e7
CAMEJO ALARCON <JUAN.CAMEJO.ALARCON.ext@ing.com>
2020-03-10 17:43:24 +0100

    Finish garaje

--- z-docs/script.js
+++ z-docs/script.js
@@ -2,6 +2,7 @@ function wonderCopy() {
 	document.getElementById('texte_a_afficher').style.userSelect = 'auto';
     document.getElementById('texte_a_afficher').style.webkitUserSelect = 'auto';
     $('#texte_a_afficher').unbind("cut copy paste");
+    copy(document.getElementById('texte_a_afficher').textContent);
 };
 
 function rocketCopy() {
