95b5fbe899bf712fdc15b21ee2aacaf2fb8c9884
juancamejoalarcon <juancamejo93@hotmail.com>
2020-03-27 21:45:36 +0100

    Finalizado contrato garaje

--- server/helpers/certified-forms/contrato-arrendamiento-plaza-garaje/information.js
+++ server/helpers/certified-forms/contrato-arrendamiento-plaza-garaje/information.js
@@ -1,37 +1,26 @@
 const information = `
-<p>
-  Entre las preguntas que se le harán a la hora de rellenar el formulario se encuentran aquellas 
-  destinadas a describir la habitación que se va a alquilar, el importe de la renta, así como establecer 
-  derechos y obligaciones entre el arrendador (propietario de la vivienda) y el arrendatario (inquilino)
-</p>
-<p>El contrato de alquiler de habitación se puede dar cuando el propietario de una vivienda, en lugar 
-  de arrendarla a un inquilino en su totalidad, alquila cada habitación por separado, mediante contratos 
-  individuales, con derecho a utilizar cocina, baño o salón de manera compartida con el resto de arrendatarios. 
-  Este propietario puede seguir viviendo en la misma reservándose el uso de una habitación.
-</p>
 <p>Antes de rellenar el formulario tenga en cuenta lo siguiente:</p>
 <ul>
-  <li>En caso de que lo que se vaya a alquilar sea toda la vivienda a varias personas, y todos ellos responden 
-    de forma conjunta de la obligación de pagar el alquiler así como de otras obligaciones (como, por ejemplo, 
-    de responder conjuntamente de los daños causados en la vivienda), lo que se debería es celebrar un contrato 
-    único de vivienda o de uso vacacional dependiendo del uso que se le vaya a dar.</li>
-  <li>Si la persona que cede la habitación al inquilino no es el propietario, sino otro inquilino que tiene un 
-    contrato de alquiler con el propietario lo que se debería hacer es formalizar un contrato de subarrendamiento.</li>
-  <li>Si la habitación se cede para usos turísticos, es necesario cumplir los requisitos de la normativa autonómica o local.</li>
-  <li>Una vez se haya rellenado el formulario, el contrato debe ser firmado por las partes intervinientes o sus representantes.
-    Si se constituyese un aval o fiador solidario (el avalista en el contrato de alquiler de vivienda o de uso distinto a vivienda
-    es el garante del cumplimiento de las obligaciones del inquilino ante el arrendador, de esta forma el arrendador tiene a quién
-    dirigirse si el inquilino incumple sus obligaciones), este también debe firmar el contrato. </li>
-  <li>El propietario debe facilitar al inquilino el certificado de eficiencia energética de la vivienda.</li>
+  <li>El contrato de alquiler de plaza de garaje es aquel contrato mediante el cual el 
+    propietario cederá el uso del espacio habilitado para aparcar un vehículo, ya sea coche 
+    o moto, de manera permanente al arrendatario.
+  </li>
+  <li>Durante la vigencia del contrato será el arrendatario la única persona que pueda aparcar en dicha plaza de aparcamiento, 
+    a cambio de pagar una renta que se habrá pactado entre las partes con carácter previo a la firma del contrato.</li>
 </ul>
 <br>
-<h3>Legislación aplicable</h3>
-
-<p> El contrato de arrendamiento de una habitación se 
-  regulará en primer lugar por lo pactado por las partes y en su defecto por las normas del Código Civil 
-  en materia de arrendamientos (arts. 1554 y siguientes del Código Civil). El certificado de eficiencia energética 
-  obligatorio se regula en el Real Decreto 235/2013, de 5 de abril, por el que se aprueba el procedimiento básico 
-  para la certificación de la eficiencia energética de los edificios.
+<p>El propietario, previo a la firma del contrato, debe mostrar al nuevo inquilino, el documento que acredite que 
+  es titular, o está en posesión de la plaza de garaje que va a ser objeto de arrendamiento.
+</p>
+<p>
+  Una vez que haya rellenado el formulario con la información necesaria, 
+  las partes deberán firmar el documento. Para un mayor nivel de seguridad, 
+  las firmas han de figurar no sólo en la sección final del documento, sino también en cada página del contrato.
+</p>
+<p>
+  Este contrato dura lo que establezcan las partes. Si en el momento de finalizar el contrato las partes 
+  no hubiesen comunicado su deseo de no renovar el contrato, éste se prorrogará por el mismo plazo que se 
+  hubiese establecido inicialmente de manera automática, sin necesidad de comunicarlo a la otra parte.
 </p>
 <br>
 <br>
