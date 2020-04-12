cb8c54b452b7e1b2b8bcdefa36a8ca892a287fd7
juancamejoalarcon <juancamejo93@hotmail.com>
2020-04-12 18:41:34 +0200

    Created sitemap

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,14 @@ const isProduction = process.env.NODE_ENV === 'production',
       isDevelopment = process.env.NODE_ENV === 'development',
       isLocal = (process.env.NODE_ENV === 'local' || process.env.NODE_ENV === 'local:windows') ? true : false;
 
-if (isLocal || isProduction || isDevelopment) {
+if (isLocal || isProduction) {
     router.use('/robots.txt', function (req, res, next) {
         res.sendFile(path.join(__dirname + '/../robots.txt'));
     });
 }
+
+router.use('/sitemap.xml', function (req, res, next) {
+    res.sendFile(path.join(__dirname + '/../sitemap.xml'));
+});
 
 module.exports = router;
