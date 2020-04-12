8b8bb539cea3bb0808d5bc94209b0540aef9854f
juancamejoalarcon <juancamejo93@hotmail.com>
2020-04-12 18:15:05 +0200

    Robots txt

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
+const path = require('path');
 
 router.use('/api', require('./api'));
+
+const isProduction = process.env.NODE_ENV === 'production',
+      isDevelopment = process.env.NODE_ENV === 'development',
+      isLocal = (process.env.NODE_ENV === 'local' || process.env.NODE_ENV === 'local:windows') ? true : false;
+
+if (isLocal || isProduction) {
+    router.use('/robots.txt', function (req, res, next) {
+        res.sendFile(path.join(__dirname + '/../robots.txt'));
+    });
+}
 
 module.exports = router;
