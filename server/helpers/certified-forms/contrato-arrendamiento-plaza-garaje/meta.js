21d4c1ed20b38a68e35bcc43afc1da4cc24ceb2e
Juan Camejo Alarcón <juancamejoalarcon@MBP-de-Juan.home>
2020-05-24 19:45:07 +0200

    Added alquiler plaza de garaje blog

---
+++ server/helpers/certified-forms/contrato-arrendamiento-plaza-garaje/meta.js
@@ -0,0 +1,35 @@
+const meta = {
+    tags: [
+        {
+            name: 'title',
+            content: 'Contrato de alquiler de plaza de garaje'
+        },
+        {
+            name: 'description',
+            content: 'El contrato de alquiler de plaza de garaje es aquel contrato mediante el cual el propietario cederá el uso del espacio habilitado para aparcar un vehículo, ya sea coche o moto, de manera permanente al arrendatario.'
+        },
+        {
+            name: 'keywords',
+            content: 'contrato alquiler plaza de garaje, contrato arrendamiento plaza de garaje,'
+        },
+        {
+            name: 'canonical',
+            href: '/static/modelos/contrato-arrendamiento-plaza-garaje'
+        },
+    ],
+    ldJson: {
+        "@context": "http://schema.org/",
+        "@type": "Product",
+        name: "contrato-arrendamiento-plaza-garaje",
+        description: "El contrato de alquiler de plaza de garaje es aquel contrato mediante el cual el propietario cederá el uso del espacio habilitado para aparcar un vehículo, ya sea coche o moto, de manera permanente al arrendatario.",
+        aggregateRating: {
+            "@type": "AggregateRating",
+            ratingValue: 4.7,
+            bestRating: 5,
+            worstRating: 0,
+            ratingCount: 5
+        }
+    }
+}
+
+module.exports = meta;
