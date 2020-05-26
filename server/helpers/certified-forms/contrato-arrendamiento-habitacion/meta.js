4c9c96e04015604dba178802af694a79fa8a9a98
Juan Camejo Alarcón <juancamejoalarcon@MBP-de-Juan.home>
2020-05-26 16:02:30 +0200

    Arrendamiento de habi blog

---
+++ server/helpers/certified-forms/contrato-arrendamiento-habitacion/meta.js
@@ -0,0 +1,35 @@
+const meta = {
+    tags: [
+        {
+            name: 'title',
+            content: 'Contrato de arrendamiento de habitación'
+        },
+        {
+            name: 'description',
+            content: 'Por medio de este contrato el propietario de una vivienda (persona o empresa) cede el uso de una de las habitaciones a un particular (persona o empresa) durante un tiempo determinado a cambio del pago de una renta.'
+        },
+        {
+            name: 'keywords',
+            content: 'contrato de arrendamiento de habitación, contrato de alquiler de habitación'
+        },
+        {
+            name: 'canonical',
+            href: '/static/modelos/contrato-arrendamiento-habitacion'
+        },
+    ],
+    ldJson: {
+        "@context": "http://schema.org/",
+        "@type": "Product",
+        name: "contrato-arrendamiento-habitacion",
+        description: "Por medio de este contrato el propietario de una vivienda (persona o empresa) cede el uso de una de las habitaciones a un particular (persona o empresa) durante un tiempo determinado a cambio del pago de una renta.",
+        aggregateRating: {
+            "@type": "AggregateRating",
+            ratingValue: 4.7,
+            bestRating: 5,
+            worstRating: 0,
+            ratingCount: 5
+        }
+    }
+}
+
+module.exports = meta;
