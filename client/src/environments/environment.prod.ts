847461a4cfe5cb8ce0abbe6c49160dfd450a1dc2
Juan Camejo Alarcón <juancamejo93@gmail.com>
2020-06-09 20:47:47 +0200

    Build for deploy

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://localhost:4000',
+  api_url: 'https://formbaseddocs-dev.herokuapp.com',
   stripe_key: 'pk_test_Us1NHhQN6advqdoP2WRSXLlZ00Eqt1Kust'
 };
