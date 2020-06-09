ab2e8f312618557bcf2479e2ca0be05cc31308e0
Juan Camejo Alarcón <juancamejo93@gmail.com>
2020-06-09 20:51:58 +0200

    Set settings to default before or after deploy

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https://formbaseddocs-dev.herokuapp.com',
+  api_url: 'http://localhost:4000',
   stripe_key: 'pk_test_Us1NHhQN6advqdoP2WRSXLlZ00Eqt1Kust'
 };
